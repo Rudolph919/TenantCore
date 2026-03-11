b04175ee2c93d988e0936d44fac98637c6ccdef7
Rudolph919 <white.rudi@gmail.com>
2026-03-11 16:11:36 +0200

    Add API route smoke test

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,7 +1,7 @@
 <?php
 
-test('the application returns a successful response', function () {
+test('the application redirects root to api v1', function () {
     $response = $this->get('/');
 
-    $response->assertStatus(200);
+    $response->assertRedirect('/api/v1');
 });
