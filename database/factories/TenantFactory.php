427a346c4ded9304dacb4798d22910c9f32a7a0d
Rudolph919 <white.rudi@gmail.com>
2026-03-11 16:21:39 +0200

    Add Tenant model and migration

---
+++ database/factories/TenantFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Tenant;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Tenant>
+ */
+class TenantFactory extends Factory
+{
+    protected $model = Tenant::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $name = fake()->company();
+
+        return [
+            'name' => $name,
+            'slug' => str()->slug($name),
+        ];
+    }
+}
